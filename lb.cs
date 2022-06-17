0be8306b604b5377cb4f35ed86a2bdf21e0cf615
Andrey <93141508+svpra@users.noreply.github.com>
2022-06-17 22:11:26 +0300

    Update lb.cs

--- lb.cs
+++ lb.cs
@@ -2,6 +2,12 @@ using SYstem;
 
 class Program {
     static void Main(string[] args) {
-        //
+        // создаём потоки. в нашем случае их будет 6, но вы можете добавить ещё
+        Thread A = new Thread();
+        Thread B = new Thread();
+        Thread C = new Thread();
+        Thread D = new Thread();
+        Thread E = new Thread();
+        Thread F = new Thread();
     }
 }
