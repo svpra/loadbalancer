2e9405d47172234d1b8713c26ac722af57c33af0
Andrey <93141508+svpra@users.noreply.github.com>
2022-06-18 18:02:06 +0300

    Update lb.cs

--- lb.cs
+++ lb.cs
@@ -1,13 +1,36 @@
-using SYstem;
+using System;
+using System.Threading;
+using System.Collections;
+using System.IO;
+
 
 class Program {
     static void Main(string[] args) {
+        
+        static void ThreadMethod() => {
+            Console.WriteLine("Да, я поток лодблнсeра :))");
+            Thread.Sleep(5000);
+        }
+        
         // создаём потоки. в нашем случае их будет 6, но вы можете добавить ещё
-        Thread A = new Thread();
-        Thread B = new Thread();
-        Thread C = new Thread();
-        Thread D = new Thread();
-        Thread E = new Thread();
-        Thread F = new Thread();
+        Thread A = new Thread(new ThreadStart(ThreadMethod));
+        Thread B = new Thread(new ThreadStart(ThreadMethod));
+        Thread C = new Thread(new ThreadStart(ThreadMethod));
+        Thread D = new Thread(new ThreadStart(ThreadMethod));
+        Thread E = new Thread(new ThreadStart(ThreadMethod));
+        Thread F = new Thread(new ThreadStart(ThreadMethod));
+        
+        string[] Threads = new Threads[6] { A, B, C, D, E, F };
+        
+        Threads[0] = Thread A;
+        Threads[1] = Thread B;
+        Threads[2] = Thread C;
+        Threads[3] = Thread D;
+        Threads[4] = Thread E;
+        Threads[5] = Thread F;
+        
+        while (Threads.IsAlive != false) {
+            var Threads = Enum.Threads();
+        }
     }
 }
